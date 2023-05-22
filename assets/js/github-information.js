9a4352a1552b584fb2c81524be77ebae64e4d7eb
NDOMINEY <>
2023-05-22 20:07:56 +0000

    Add fetchGitHub function with loading gif

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,9 +1,32 @@
-function fetchGitHubInformation(event){
+function fetchGitHubInformation(){
 
-    let username = $("gh-username").val();
+    var username = $("#gh-username").val();
     
     if (!username){
-        $("gh-user-data").html("<h2>Please enter a Github Username</h2>");
+        $("#gh-user-data").html("<h2>Please enter a Github Username</h2>");
+        return;
     }
+ 
+    $("#gh-user-data").html(`
+    <div id="loader">
+        <img src="assets/images/loader.gif" alt="Loading...."/>
+    </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(function(response){
+        var userData = response;
+
+        $("#gh-user-data").html(userInformationHTML(userData));
+    }, function(errorResponse){
+        if(errorResponse.status === 404){
+            $("#gh-user-data").html(`<h2>No info found for ${username}</h2>`)
+        } else {
+            console.log(errorResponse);
+            $("#gh-user-data").html(
+                `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+        }
+    }
+    )
 
 }
